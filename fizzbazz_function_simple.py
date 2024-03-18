61935382abc7597590422de562e19440cc12f336
Johan Rodriguez <rodriguezjpv1@gmail.com>
2024-03-19 00:28:12 +0100

    renombrando

--- fizzbazz_function_simple.py
+++ fizzbazz_function_simple.py
@@ -1,11 +1,11 @@
 def fizzbuzz(your_num):
     for num in range(1, your_num + 1):#define rango, suma 1 para incluir valor ingresado
         if num%3 == 0 and num%5 ==0: #para verificar primero si es divisible entre 5 y 3
-            print('fizzbuzz')
+            print('FizzBuzz')
         elif num%3 == 0:
-            print('fizz')
+            print('Fizz')
         elif num%5 ==0:
-            print('buzz')
+            print('Buzz')
         else:
             print(num)
-fizzbuzz(20)
+fizzbuzz(100)
