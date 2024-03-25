e82a68500a6824c036eb14c0080f6859a9bcbdb1
Johan Rodriguez <rodriguezjpv1@gmail.com>
2024-03-25 08:47:10 +0100

    otro

--- fizzbazz_function_simple.py
+++ fizzbazz_function_simple.py
@@ -1,11 +1,13 @@
+import math #para importar la libreria que necesito para los modulos
 def fizzbuzz(your_num):
-    for num in range(1, your_num + 1):#define rango, suma 1 para incluir valor ingresado
+    for num in range(1, your_num + 1):# define el rango, utilizando el valor ingresado, le suma uno por el index
         if num%3 == 0 and num%5 ==0: #para verificar primero si es divisible entre 5 y 3
-            print('FizzBuzz')
+            print('fizzbuzz') 
         elif num%3 == 0:
-            print('Fizz')
+            print('fizz')
         elif num%5 ==0:
-            print('Buzz')
+            print('buzz')
         else:
             print(num)
-fizzbuzz(100)
+
+fizzbuzz(15)
